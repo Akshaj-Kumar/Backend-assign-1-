3ec63a94653af96463d8c9e1316cf576dfd6bd24
Akshaj-Kumar <akshajgarg0047@gmail.com>
2026-08-30 09:00:56 +0530

    commit update

--- modules/isEven.js
+++ modules/isEven.js
@@ -1,6 +1,4 @@
-// modules/isEven.js
-// Custom module: checks whether a given number is even or odd.
-// Demonstrates module creation and reusability using module.exports.
+
 
 function isEven(number) {
   return number % 2 === 0;
